c2419079aedfe028ecddef994701b011c4e0f6e3
Peter Zotov <whitequark@whitequark.org>
2012-09-21 17:38:37 +0400

    Add Liquor ActionController renderer.

--- lib/liquor/extensions/rails.rb
+++ lib/liquor/extensions/rails.rb
@@ -16,3 +16,20 @@ module Liquor
     end
   end
 end
+
+ActionController::Renderers.add :liquor do |options, *|
+  html = nil
+  manager, name, layout_name, environment = \
+        options.values_at(:manager, :template, :layout, :environment)
+
+  ActiveSupport::Notifications.instrument("render_template.action_view",
+        identifier: name, layout: layout_name) do
+    if layout_name
+      html = manager.render_with_layout(layout_name, environment, name, environment)
+    else
+      html = manager.render(name, environment)
+    end
+  end
+
+  render :text => html
+end
